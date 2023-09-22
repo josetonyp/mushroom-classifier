86a27e3457fee77eef44bdd2938ef8da1d4da1e5
Jose Antonio Pio Gil <jose.pio@latizana.com>
2023-09-22 13:19:06 +0200

    - [x] The Image Folder Generator inpects jpeg to discards images that are broken internally.

--- source/cnn/generators/image_folder_generator.py
+++ source/cnn/generators/image_folder_generator.py
@@ -5,6 +5,43 @@ from glob import glob
 from tensorflow.io import read_file
 from tensorflow.image import decode_jpeg, resize
 
+from struct import unpack
+from tqdm import tqdm
+import os
+
+
+class JPEGInspector:
+    MARKERS = {
+        0xFFD8: "Start of Image",
+        0xFFE0: "Application Default Header",
+        0xFFDB: "Quantization Table",
+        0xFFC0: "Start of Frame",
+        0xFFC4: "Define Huffman Table",
+        0xFFDA: "Start of Scan",
+        0xFFD9: "End of Image",
+    }
+
+    def __init__(self, image_file):
+        with open(image_file, "rb") as f:
+            self.img_data = f.read()
+
+    def decode(self):
+        data = self.img_data
+        while True:
+            (marker,) = unpack(">H", data[0:2])
+            # print(marker_mapping.get(marker))
+            if marker == 0xFFD8:
+                data = data[2:]
+            elif marker == 0xFFD9:
+                return
+            elif marker == 0xFFDA:
+                data = data[-2:]
+            else:
+                (lenchunk,) = unpack(">H", data[2:4])
+                data = data[2 + lenchunk :]
+            if len(data) == 0:
+                break
+
 
 class ImageFolderGenerator:
     def __init__(self, preprocess_input_method=None):
@@ -24,6 +61,23 @@ class ImageFolderGenerator:
         df["label"] = df["nameLabel"].replace(
             df.nameLabel.unique(), [*range(len(df.nameLabel.unique()))]
         )
+
+        ## Remove images that have internal errors
+        bads = []
+        print("Searching for broken JPG Images")
+        for filepath in tqdm(df.filepath):
+            image = JPEGInspector(filepath)
+            try:
+                image.decode()
+            except:
+                bads.append(filepath)
+
+        if bads != []:
+            print("Removing the following images from the dataset")
+            for bad in bads:
+                print(f"- {bad}")
+                df.drop(df.loc[df["filepath"] == bad].index, inplace=True)
+
         dataset_train = tensorflow.data.Dataset.from_tensor_slices(
             (df.filepath, df.label)
         )
