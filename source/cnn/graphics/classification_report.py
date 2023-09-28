6c35e961ee3b700d6f78cf82bf32d62a7258de95
Jose Antonio Pio Gil <jose.pio@latizana.com>
2023-09-28 16:34:12 +0200

    - [x] Adds command to recreate graphics - [x] Adds environment variable to log only errors in tensorflow - [x] Refactor Confusion Matrix and Clasification Report to render Label Names

--- source/cnn/graphics/classification_report.py
+++ source/cnn/graphics/classification_report.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import re, itertools
+from matplotlib.colors import LinearSegmentedColormap
+
+grg = LinearSegmentedColormap.from_list("rg", ["#EAEAF2", "#3174A1"], N=256)
 
 
 class ClassificationReport:
-    def __init__(self, report, model_name, subtitle=""):
+    def __init__(self, report, model_name, subtitle="", label_names=None):
         self.report = report
         self.model_name = model_name
         self.subtitle = subtitle
+        self.label_names = label_names
 
-    def render(self, title="Classification report ", cmap="RdBu", class_names=None):
+    def render(
+        self,
+        title="Classification report ",
+        cmap=grg,
+        class_names=None,
+        figsize=(50, 50),
+    ):
         """
         Plot scikit-learn classification report.
         Extension based on https://stackoverflow.com/a/31689645/395857
@@ -24,27 +34,46 @@ class ClassificationReport:
             nums = [float(x) for x in nums]
             matrix += [nums]
 
-        self.fig, ax = plt.subplots(figsize=(15, 10))
+        plt.rcParams["font.family"] = "Optima LT Std"
+        side_size, _ = figsize
+
+        self.fig, ax = plt.subplots(figsize=(side_size, side_size))
 
         self.fig.suptitle(
-            f"Classification Report Model {self.model_name.upper()}", fontsize=24
+            f"Classification Report Model {self.model_name.upper()}",
+            fontsize=side_size * 1.5,
         )
         if self.subtitle != "":
-            ax.set_title(self.subtitle, fontsize=15)
+            ax.set_title(self.subtitle, fontsize=side_size * 1.5)
+            ax.title.set_color("darkgreen")
 
         matrix = np.array(matrix)
         values = matrix[:, 1:]
 
-        im = ax.imshow(values, interpolation="nearest", cmap="Blues")
-        ax.set_xlabel("Metrics")
-        ax.set_ylabel("Classes")
+        im = ax.imshow(values, interpolation="nearest", cmap=cmap)
+        ax.set_xlabel("Metrics", fontsize=side_size * 1.5)
+        ax.set_ylabel("Classes", fontsize=side_size * 1.5)
 
-        xtick_marks = np.arange(len(values[0]))
-        ax.set_xticks(xtick_marks, ["Precision", "Recall", "F1-score", "support"])
+        if self.label_names != None:
+            ax.set_yticks(
+                np.arange(len(matrix)),
+                self.label_names,
+                fontsize=side_size * 1.5,
+            )
+        else:
+            ax.set_yticks(
+                np.arange(len(matrix)),
+                [int(x) for x in matrix[:, 0]],
+                fontsize=side_size * 1.5,
+            )
 
-        ytick_marks = np.arange(len(matrix))
-        ax.set_yticks(ytick_marks, [int(x) for x in matrix[:, 0]])
+        ax.set_xticks(
+            np.arange(len(values[0])),
+            ["Precision", "Recall", "F1-score", "support"],
+            fontsize=side_size * 1.5,
+        )
 
+        ax.grid(False)
         for i, j in itertools.product(range(values.shape[0]), range(values.shape[1])):
             im.axes.text(
                 j,
@@ -52,6 +81,7 @@ class ClassificationReport:
                 values[i, j],
                 horizontalalignment="center",
                 color="white" if values[i, j] > (values.max() / 2) else "black",
+                fontsize=side_size * 2,
             )
 
         return self
--- source/cnn/graphics/confussion_matrix.py
+++ source/cnn/graphics/confussion_matrix.py
@@ -3,27 +3,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import itertools
 
+from matplotlib.colors import LinearSegmentedColormap
+
+grg = LinearSegmentedColormap.from_list("rg", ["#EAEAF2", "#3174A1"], N=256)
+
 
 class ConfusionMatrix:
-    def __init__(self, matrix, model_name, subtitle=""):
+    def __init__(self, matrix, model_name, subtitle="", label_names=None):
         self.matrix = np.array(matrix)
         self.classes = range(0, len(matrix))
         self.model_name = model_name
         self.subtitle = subtitle
+        self.label_names = label_names
 
-    def render(self, figsize=(12, 12)):
+    def render(
+        self,
+        cmap=grg,
+        figsize=(50, 50),
+    ):
+        plt.rcParams["font.family"] = "Optima LT Std"
+        side_size, _ = figsize
         self.fig, ax = plt.subplots(figsize=figsize)
+        ax.grid(False)
         self.fig.suptitle(
-            f"Confusion Matrix Model {self.model_name.upper()}", fontsize=24
+            f"Confusion Matrix Model {self.model_name.upper()}",
+            fontsize=side_size * 1.5,
         )
         if self.subtitle != "":
-            ax.set_title(self.subtitle, fontsize=15)
+            ax.set_title(self.subtitle, fontsize=side_size * 1.5)
 
-        im = ax.imshow(self.matrix, interpolation="nearest", cmap="Blues")
+        im = ax.imshow(self.matrix, interpolation="nearest", cmap=cmap)
         self.fig.colorbar(im, ax=ax)
         tick_marks = np.arange(len(self.classes))
-        ax.set_xticks(tick_marks, self.classes)
-        ax.set_yticks(tick_marks, self.classes)
+        if self.label_names != None:
+            ax.set_xticks(tick_marks, self.label_names, fontsize=side_size * 1.5)
+            ax.set_yticks(tick_marks, self.label_names, fontsize=side_size * 1.5)
+        else:
+            ax.set_xticks(tick_marks, tick_marks, fontsize=side_size * 1.5)
+            ax.set_yticks(tick_marks, tick_marks, fontsize=side_size * 1.5)
 
         for i, j in itertools.product(
             range(self.matrix.shape[0]), range(self.matrix.shape[1])
@@ -36,10 +53,11 @@ class ConfusionMatrix:
                 color="white"
                 if self.matrix[i, j] > (self.matrix.max() / 2)
                 else "black",
+                fontsize=side_size * 2,
             )
 
-        ax.set_ylabel("True Labels")
-        ax.set_xlabel("Predicted Labels")
+        ax.set_ylabel("True Labels", fontsize=side_size * 1.5)
+        ax.set_xlabel("Predicted Labels", fontsize=side_size * 1.5)
 
         return self
 
