6958d55d6d02107309e7d733f98f29b5fbfe2375
Jose Antonio Pio Gil <jose.pio@latizana.com>
2023-10-06 20:53:18 +0200

    Adds Psilocybe and Mushrooms Predictors

--- streamlit.py
+++ streamlit.py
@@ -15,6 +15,8 @@ pages = [
     "Flower Set 1",
     "Flower Set 2",
     "Mushrooms Set 1",
+    "Mushrooms Psilocybe",
+    "Mushrooms Observer",
 ]
 page = st.sidebar.radio("Index", pages)
 
@@ -33,25 +35,54 @@ def load_image_url(url):
     return np_image
 
 
-img = None
+def render_images(folder):
+    df = pd.read_csv(f"{folder}/images.csv")
+    features = []
+    for file in df.feature:
+        fc = file.split("/")
+        features.append(f"{folder}/{fc[-2]}/{fc[-1]}")
+
+    return image_select(
+        "Images",
+        features,
+        captions=list(df.label_name.values),
+        use_container_width=False,
+    )
+
+
+def make_sorter(l):
+    """
+    Create a dict from the list to map to 0..len(l)
+    Returns a mapper to map a series to this custom sort order
+    """
+    sort_order = {k: v for k, v in zip(l, range(len(l)))}
+    return lambda s: s.map(lambda x: sort_order[x])
+
+
+def render_images_df(folder, classes):
+    df = pd.read_csv(f"{folder}/images.csv")
+    df = df[df.label.isin(classes)]
+    df = df.sort_values("label", key=make_sorter(classes))
+
+    features = []
+    for file in df.image_lien:
+        features.append(f"{folder}/images/{file}")
+
+    return image_select(
+        "Images",
+        features,
+        captions=list(df.label.values),
+        use_container_width=False,
+    )
+
+
 if page == pages[0]:
     st.title("Flower Set 1")
 
     assets = "static/flower_photos"
     model_flower_photos = load_model(f"{assets}/model.keras")
-    df = pd.read_csv(f"{assets}/images.csv")
 
-    features = []
-    for file in df.feature:
-        fc = file.split("/")
-        features.append(f"{assets}/{fc[-2]}/{fc[-1]}")
-
-    img_feature = image_select(
-        "Flowers",
-        features,
-        captions=list(df.label_name.values),
-        use_container_width=False,
-    )
+    img_feature = render_images(assets)
     if img_feature != None:
         img = load_image(img_feature)
         predictions = model_flower_photos.predict(img)
@@ -69,19 +100,8 @@ if page == pages[1]:
 
     assets = "static/flower_images"
     model_flower_images = load_model(f"{assets}/model.keras")
-    df = pd.read_csv(f"{assets}/images.csv")
 
-    features = []
-    for file in df.feature:
-        fc = file.split("/")
-        features.append(f"{assets}/{fc[-2]}/{fc[-1]}")
-
-    img_feature = image_select(
-        "Flowers",
-        features,
-        captions=list(df.label_name.values),
-        use_container_width=False,
-    )
+    img_feature = render_images(assets)
     if img_feature != None:
         img = load_image(img_feature)
         predictions = model_flower_images.predict(img)
@@ -104,19 +124,8 @@ if page == pages[2]:
 
     assets = "static/Kaggle Mushrooms"
     model_mushset1 = load_model(f"{assets}/model.keras")
-    df = pd.read_csv(f"{assets}/images.csv")
 
-    features = []
-    for file in df.feature:
-        fc = file.split("/")
-        features.append(f"{assets}/{fc[-2]}/{fc[-1]}")
-
-    img_feature = image_select(
-        "Flowers",
-        features,
-        captions=list(df.label_name.values),
-        use_container_width=False,
-    )
+    img_feature = render_images(assets)
     if img_feature != None:
         img = load_image(img_feature)
         predictions = model_mushset1.predict(img)
@@ -136,4 +145,91 @@ if page == pages[2]:
             "Russula",
             "Amanita",
         ]
+        st.write(names[predictions])
+
+if page == pages[3]:
+    st.title("Psilocybe Images Set")
+    names = [
+        "Psilocybe zapotecorum",
+        "Psilocybe cyanescens",
+        "Psilocybe allenii",
+        "Psilocybe ovoideocystidiata",
+        "Psilocybe pelliculosa",
+        "Psilocybe caerulescens",
+        "Psilocybe neoxalapensis",
+        "Psilocybe cubensis",
+        "Psilocybe stuntzii",
+        "Psilocybe azurescens",
+        "Psilocybe baeocystis",
+        "Deconica coprophila",
+        "Psilocybe semilanceata",
+        "Psilocybe muliercula",
+        "Psilocybe caerulipes",
+        "Psilocybe mexicana",
+        "Psilocybe yungensis",
+        "Psilocybe aztecorum",
+        "Psilocybe subaeruginosa",
+        "Psilocybe fagicola",
+    ]
+
+    n_class_select = st.radio(
+        "Select the number of Classes to load", ["3 Classes", "5 Classes"]
+    )
+
+    if n_class_select == "3 Classes":
+        n_class = 3
+    else:
+        n_class = 5
+
+    assets = "static/psilocybe"
+
+    img_feature = render_images_df(assets, names[:n_class])
+    model_psilocybe = load_model(f"{assets}/model_{n_class}.keras")
+
+    if img_feature != None:
+        img = load_image(img_feature)
+        predictions = model_psilocybe.predict(img)
+        predictions = np.argmax(predictions, axis=1)[0]
+        print(predictions)
+        print(img_feature)
+
+        st.image(load_image(img_feature) / 255)
+        st.write(names[predictions])
+
+if page == pages[4]:
+    st.title("Mushrooms Observer Set")
+    names = [
+        "Agaricales",
+        "Russula",
+        "Cortinarius",
+        "Amanita",
+        "Polyporales",
+        "Psathyrella",
+        "Agaricus",
+        "Inocybe",
+        "Mycena",
+        "Entoloma",
+    ]
+
+    n_class_select = st.radio(
+        "Select the number of Classes to load", ["3 Classes", "5 Classes"]
+    )
+
+    if n_class_select == "3 Classes":
+        n_class = 3
+    else:
+        n_class = 5
+
+    assets = "static/mushrooms"
+    model_musshy = load_model(f"{assets}/model_{n_class}.keras")
+    img_feature = render_images_df(assets, names[:n_class])
+
+    if img_feature != None:
+        img = load_image(img_feature)
+        predictions = model_musshy.predict(img)
+        predictions = np.argmax(predictions, axis=1)[0]
+        print(predictions)
+        print(img_feature)
+
+        st.image(load_image(img_feature) / 255)
         st.write(names[predictions])
