e4a13ba9959e40391162d17cf28e09528073ae81
Jose Antonio Pio Gil <jose.pio@latizana.com>
2023-10-07 13:39:52 +0200

    - [x] - [x] - [x]

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
-# mushroom-classifier
-Mushrooms Classifier
+# Mushroom Classifier CLI App
+
+The objective of this project is to present a Command Line Application as a Framework to automate the training of several Convolutional Networks and document the output of the training and evaluation process. The training standarize the execution of CNN for computer vision and object classifications. 
+
+
+## Datasets Used
+
+The trainig process was run in the following datasets:
+
+### **Flower Images:** 
+
+A balanced 5 classes flower images data set disposed into 5 folders with the name of the class/flower as folder name. No CSV file present. 
+
+URL: https://www.kaggle.com/datasets/kausthubkannan/5-flower-types-classification-dataset
+
+![Flower Images Data Set](/static/flower_images/samples2.jpg "Flower Images Data Set")
+
+
+### **Flower Photos:** 
+
+An unbalanced 5 classes flower images data set disposed into 5 folders with the name of the class/flower as folder name. No CSV file present. 
+
+URL: https://www.tensorflow.org/datasets/catalog/tf_flowers
+
+![Flower Photos Data Set](/static/flower_photos/samples2.jpg "Flower Photos Data Set")
+
+### **Kaggle Mushroom Images Dataset:** 
+
+An unbalanced 9 class mushrooms images dataset disposed into 9 folders with the name of each mushroom as a folder name. No CSV file present.
+
+URL: https://www.kaggle.com/datasets/maysee/mushrooms-classification-common-genuss-images
+
+![Kaggle Mushroom Images Dataset](/static/psilocybe/samples.jpg)
+
+
+### **Mushroom Observer's Images Dataset:** 
+
+A considerable big database of 648_000 images divided in aproximately 1_000 especies. The Database is well documented biologically and location based.  
+
+
+URL: https://mushroomobserver.org/
+
+![Kaggle Mushroom Images Dataset](/static/mushrooms/samples.jpg)
+
+
+## Install
+
+To install the CLI App clone the repositor in your folder
+
+```bash
+git clone https://github.com/josetonyp/mushroom-classifier.git
+mkdir models # Output folder where models will be documented
+mkdir input # Folder where to locate images to train the model
+```
+
+
+## Execute
+
+### Trainers
+
+###### Folders DataSets
+
+```bash
+
+```
+
+###### CSV DataSets
+
+```bash
+
+```
+
+### Renderers
+
+Convertst the classification report, confussion matrix and training history from the training evaluation into an image. 
+
+###### Render a Classification Report
+
+Reads the classification report files from the training ouput folder and converts it into an image
+
+```bash
+./learning render-report \
+            --file_name=<path_to_file> \
+            --title="Desired Title" \
+            --label_names="<Comma,separated,label,names>" \
+            --figsize=60,60
+```
+###### Render a Confusion Matrix
+
+```bash
+
+```
+
+###### Render a Training History
+
+```bash
+
+```
+
+
+## Test
+The current architecture is build as POC and there is no intention to Unit Test the code.
+
+
+## Collaboration and Updates
+
+The code is deliver as it is. We do not have plans to upgrade this code commercially beyond our Data Science studies but we are open to help you understand and improve given our availability. The code has been tested in Mac and Linux only and only. The Tensorflow design was run only in CPU with lmited capacities. 
+
+
+## Licence
+
+#### MIT License
+
+Copyright (c) 2023 [Jose Pio](https://github.com/josetonyp) and [Sushovan Dam](https://github.com/sdam89)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- source/cnn/graphics/classification_report.py
+++ source/cnn/graphics/classification_report.py
@@ -1,38 +1,53 @@
+from __future__ import annotations
 import matplotlib.pyplot as plt
 import numpy as np
 import re, itertools
+
+from functools import cached_property
+
 from matplotlib.colors import LinearSegmentedColormap
 
 grg = LinearSegmentedColormap.from_list("rg", ["#EAEAF2", "#3174A1"], N=256)
 
 
 class ClassificationReport:
-    def __init__(self, report, model_name, subtitle="", label_names=None):
-        self.report = report
-        self.model_name = model_name
-        self.subtitle = subtitle
-        self.label_names = label_names
+    """Reads a Keras Classfication Report and Renders a confusion matrix style
+    image from that report
+
+    Example:
+    ClassificationReport(data: str, title: str, label_names: list | None =label_names)\
+        .render(figsize=(<int>,<int>))\
+        .save("<path_to_file>")
+    """
+
+    def __init__(
+        self,
+        report: str,
+        title: str = "",
+        subtitle: str = "",
+        label_names: list | None = None,
+    ) -> None:
+        self.__report = report
+        self.__title = title
+        self.__subtitle = subtitle
+        self.__label_names = label_names
 
     def render(
         self,
-        title="Classification report ",
-        cmap=grg,
-        class_names=None,
-        figsize=(50, 50),
-    ):
-        """
-        Plot scikit-learn classification report.
-        Extension based on https://stackoverflow.com/a/31689645/395857
-        """
-        lines = self.report.split("\n")
+        cmap: LinearSegmentedColormap = grg,
+        figsize: tuple = (50, 50),
+    ) -> ClassificationReport:
+        """Plots scikit-learn classification report.
+        Based on https://stackoverflow.com/a/31689645/395857
 
-        matrix = []
-        for result in lines[2:]:
-            if result.strip() == "":
-                break
-            nums = re.findall("[\d\.]+", result.strip())
-            nums = [float(x) for x in nums]
-            matrix += [nums]
+
+        Args:
+            cmap (LinearSegmentedColormap, optional): Color Map. Defaults to LinearSegmentedColormap.from_list("rg", ["#EAEAF2", "#3174A1"], N=256).
+            figsize (tuple, optional): Figure Size. Defaults to (50, 50).
+
+        Returns:
+            ClassificationReport: Instance of the renderer
+        """
 
         plt.rcParams["font.family"] = "Optima LT Std"
         side_size, _ = figsize
@@ -40,24 +55,26 @@ class ClassificationReport:
         self.fig, ax = plt.subplots(figsize=figsize)
 
         self.fig.suptitle(
-            f"Classification Report {self.model_name.upper()}",
+            f"Classification Report {self.__title}",
             fontsize=side_size * 1.5,
         )
-        if self.subtitle != "":
-            ax.set_title(self.subtitle, fontsize=side_size * 1.5)
-            ax.title.set_color("darkgreen")
+        ax.set_title(
+            self.__subtitle + f"Total Accuracy: {self.accuracy}",
+            fontsize=side_size * 1.5,
+        )
+        ax.title.set_color("dimgray")
 
-        matrix = np.array(matrix)
+        matrix = self.classification_matrix
         values = matrix[:, 1:]
 
         im = ax.imshow(values, interpolation="nearest", cmap=cmap)
         ax.set_xlabel("Metrics", fontsize=side_size * 1.5)
         ax.set_ylabel("Classes", fontsize=side_size * 1.5)
 
-        if self.label_names != None:
+        if self.__label_names != None:
             ax.set_yticks(
                 np.arange(len(matrix)),
-                self.label_names,
+                self.__label_names,
                 fontsize=side_size * 1.5,
             )
         else:
@@ -86,5 +103,58 @@ class ClassificationReport:
 
         return self
 
-    def save(self, filename):
-        self.fig.savefig(filename)
+    def save(self, filename: str) -> None:
+        """Save the generated figure into a file
+
+        Args:
+            filename (str): Target file name
+        """
+        self.fig.savefig(filename, dpi=200)
+
+    @cached_property
+    def lines(self) -> list:
+        """Converts the text report into a list of individual lines
+
+        Returns:
+            list: Report lines
+        """
+        return self.__report.split("\n")
+
+    @cached_property
+    def classification_matrix(self) -> np.ndarray:
+        """Extracts classification precision, recall, f1-score and support from report
+        and converst it into a renderable numpay array.
+
+        Returns:
+            np.ndarray: Report values
+        """
+        matrix = []
+
+        for result in self.lines[2:]:
+            if result.strip() == "":
+                break
+            nums = re.findall("[\d\.]+", result.strip())
+            nums = [float(x) for x in nums]
+            matrix += [nums]
+
+        return np.array(matrix)
+
+    @cached_property
+    def accuracy(self) -> float:
+        """Extracts the total report accuracy from the report
+
+        Returns:
+            float: Report total accuracy
+        """
+        accuracy = self.lines[2:]
+
+        breking_index = 0
+        for i, result in enumerate(self.lines[2:]):
+            if result.strip() == "":
+                breking_index = i
+                break
+
+        accuracy_line = self.lines[2:][breking_index + 1]
+
+        nums = re.findall("[\d\.]+", accuracy_line.strip())
+        return float(nums[0])
