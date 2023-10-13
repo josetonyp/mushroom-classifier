92cde3cee7f328922a9fc33e9b80f741b3f26fd9
Jose Antonio Pio Gil <jose.pio@latizana.com>
2023-10-13 12:24:47 +0200

    - [x] Install Pylintrc from google, extend for 4 spaces - [x] Lint and document Logger and Predictor classes

--- source/cnn/predictor.py
+++ source/cnn/predictor.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import json
 
 import numpy as np
@@ -12,16 +13,54 @@ from tensorflow.keras.models import load_model
 tf.get_logger().setLevel("INFO")
 
 
-class Predictor:
-    def __init__(self, batch_size=64):
+class Predictor(object):
+    """
+    A class for making predictions using a Keras model.
+
+    Attributes:
+        model (keras.models.Model): The Keras model to use for predictions.
+        generator (object): The image
+        data generator to use for predictions.
+        batch_size (int): The batch size to use for predictions.
+    """
+
+    def __init__(self, batch_size: int = 64):
+        """
+        Initializes a new Predictor object.
+
+        Args:
+            batch_size (int): The batch size to use for predictions
+            (default: 64).
+        """
         self.__batch_size = batch_size
 
-    def load(self, model_file):
+    def load(self, model_file: str) -> Predictor:
+        """
+        Loads a Keras model from a file.
+
+        Args:
+            model_file (str): The path to the model file.
+
+        Returns:
+            self (Predictor): The Predictor object.
+        """
         self.model = load_model(model_file)
 
         return self
 
-    def predict(self, generator):
+    def predict(self, generator: object) -> Predictor:
+        """
+        Makes predictions using the loaded Keras model and the given image
+        data generator.
+
+        Args:
+            generator (object): The image data generator to use for
+            predictions.
+
+        Returns:
+            predictions (numpy.ndarray): The predicted class probabilities
+            for the input images.
+        """
         self.generator = generator
         steps = (generator.n // self.__batch_size) + 1
 
@@ -37,28 +76,55 @@ class Predictor:
         )
 
         self.predictions = np.argmax(predictions, axis=1)
+        return self
 
-    def accuracy_score(self):
+    def accuracy_score(self) -> float:
+        """
+        Computes the accuracy score of the predictions.
+
+        Returns:
+            score (float): The accuracy score.
+        """
         return accuracy_score(self.generator.labels, self.predictions)
 
-    def classification_report(self, to_file=None):
+    def classification_report(self, to_file: str | None = None) -> str:
+        """
+        Generate a classification report for the model's predictions.
+
+        Args:
+            to_file (str | None): If provided, the classification report
+            will be saved to this file.
+
+        Returns:
+            str: The classification report as a string.
+        """
         self.class_report = classification_report(
             self.generator.labels, self.predictions
         )
 
-        if to_file is None and type(to_file) is str:
-            open(to_file, "w").write(self.class_report)
+        if to_file is None and isinstance(to_file) is str:
+            open(to_file, "w", encoding="utf-8").write(self.class_report)
 
         return self.class_report
 
-    def confusion_matrix(self, to_file=None):
+    def confusion_matrix(self, to_file: str | None = None) -> str:
+        """
+        Computes the confusion matrix for the predictions made by the model.
+
+        Args:
+            to_file (str | None): If provided, saves the confusion matrix
+            as a JSON file at the given path.
+
+        Returns:
+            str: The computed confusion matrix.
+        """
         self.cnf_matrix = confusion_matrix(
             self.generator.labels,
             self.predictions,
         )
 
-        if to_file is not None and type(to_file) is str:
+        if to_file is not None and isinstance(to_file) is str:
             text = json.dumps({"matrix": self.cnf_matrix.tolist()})
-            open(to_file, "w").write(text)
+            open(to_file, "w", encoding="utf-8").write(text)
 
         return self.cnf_matrix
--- source/logger.py
+++ source/logger.py
@@ -2,12 +2,27 @@ import logging
 
 
 class Logger(object):
-    def __init__(self, output_report, level=logging.DEBUG, logger_name="cnn"):
-        self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler(output_report)
-        fh.setLevel(logging.DEBUG)
-        self.logger.addHandler(fh)
+    """Creates and configure a training logger
 
-    def get_logger(self):
-        return self.logger
+    Args:
+        object (_type_): _description_
+    """
+
+    def __init__(self, output_report, level=logging.DEBUG, logger_name="cnn"):
+        """
+        Initializes a new Logger object.
+
+        Args:
+            output_report (str): The path to the log file.
+            level (int): The logging level (default: logging.DEBUG).
+            logger_name (str): The name of the logger (default: "cnn").
+        """
+        self.__logger = logging.getLogger(logger_name)
+        self.__logger.setLevel(level)
+        fh = logging.FileHandler(output_report)
+        fh.setLevel(level)
+        self.__logger.addHandler(fh)
+
+    @property
+    def logger(self):
+        return self.__logger
